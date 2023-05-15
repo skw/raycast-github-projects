aec0f4bb0f5105074c7e37a2fc785774ae15fbd7
Shaun Kirk Wong <skw@github.com>
2023-05-14 23:01:10 -0600

    Prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { ActionPanel, List, Action, getPreferenceValues, LocalStorage } from "@raycast/api";
-import { useCallback, useEffect,  useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { graphqlClient, Project, RecentProjectsQuery, recentProjectsQuery } from "./query";
 import { Preferences } from "./preferences";
 import dayjs from "dayjs";
@@ -10,21 +10,20 @@ dayjs.extend(relativeTime);
 const { login } = getPreferenceValues<Preferences>();
 
 export default function Command() {
-  const [isLoadingRecentProjects, setIsLoadingRecentProjects] = useState<boolean>(true)
+  const [isLoadingRecentProjects, setIsLoadingRecentProjects] = useState<boolean>(true);
   const [recentProjects, setRecentProjects] = useState<Array<Project>>([]);
   const [recentlyViewedProjects, setRecentlyViewedProjects] = useState<Array<Project>>([]);
-  const [recentlyViewedProjectMap, setRecentlyViewedProjectMap] = useState<Map<string, Project>>(new Map)
-
+  const [recentlyViewedProjectMap, setRecentlyViewedProjectMap] = useState<Map<string, Project>>(new Map());
 
   useEffect(() => {
     async function fetchRecentProjects() {
       try {
-        setIsLoadingRecentProjects(true)
+        setIsLoadingRecentProjects(true);
 
         const result: RecentProjectsQuery = await graphqlClient(recentProjectsQuery, { login });
 
         setRecentProjects(result.organization.recentProjects.nodes);
-        setIsLoadingRecentProjects(false)
+        setIsLoadingRecentProjects(false);
       } catch (error) {
         console.error(error);
       }
@@ -36,28 +35,31 @@ export default function Command() {
   const onOpenProject = useCallback(async (url: string) => {
     const currentJson = await LocalStorage.getItem(`recently-viewed`);
 
-    const hash = currentJson ? JSON.parse(currentJson.toString()) : {}
+    const hash = currentJson ? JSON.parse(currentJson.toString()) : {};
 
-    LocalStorage.setItem(`recently-viewed`, JSON.stringify({
-      ...hash,
-      [url]: Date.now()
-    }));
-  }, [])
+    LocalStorage.setItem(
+      `recently-viewed`,
+      JSON.stringify({
+        ...hash,
+        [url]: Date.now(),
+      })
+    );
+  }, []);
 
   useEffect(() => {
     async function parseRecentlyViewedProjects() {
       try {
         const currentJson = await LocalStorage.getItem(`recently-viewed`);
 
-        if (!currentJson) return
+        if (!currentJson) return;
 
-        const hash = JSON.parse(currentJson.toString())
+        const hash = JSON.parse(currentJson.toString());
 
-        setRecentlyViewedProjects(recentProjects
-          .filter(project => hash[project.url])
-          .sort())
+        setRecentlyViewedProjects(recentProjects.filter((project) => hash[project.url]).sort());
 
-        setRecentlyViewedProjectMap(new Map(recentProjects.filter(project => hash[project.url]).map(project => [project.url, project])))
+        setRecentlyViewedProjectMap(
+          new Map(recentProjects.filter((project) => hash[project.url]).map((project) => [project.url, project]))
+        );
       } catch (error) {
         console.error(error);
       }
@@ -66,13 +68,14 @@ export default function Command() {
     parseRecentlyViewedProjects();
   }, [recentProjects]);
 
-
- console.log("recentlyViewedProjects ", recentlyViewedProjects.length);
-
-
   return (
-    <List navigationTitle="Search Projects" searchBarPlaceholder="Search your Projects" isShowingDetail isLoading={isLoadingRecentProjects}>
-      {recentlyViewedProjects.length > 0  ? (
+    <List
+      navigationTitle="Search Projects"
+      searchBarPlaceholder="Search your Projects"
+      isShowingDetail
+      isLoading={isLoadingRecentProjects}
+    >
+      {recentlyViewedProjects.length > 0 ? (
         <List.Section title="Recently Viewed">
           {recentlyViewedProjects.map((p) => (
             <List.Item
@@ -104,38 +107,40 @@ export default function Command() {
             />
           ))}
         </List.Section>
-      ) :  null}
+      ) : null}
       {recentProjects.length > 0 && !isLoadingRecentProjects ? (
         <List.Section title="Recently Updated Projects">
-          {recentProjects.filter(p => !recentlyViewedProjectMap.get(p.url)).map((p) => (
-            <List.Item
-              key={p.id}
-              title={p.title}
-              icon={{ source: "table.svg" }}
-              detail={
-                <List.Item.Detail
-                  markdown={p.shortDescription}
-                  metadata={
-                    <List.Item.Detail.Metadata>
-                      <List.Item.Detail.Metadata.Label title="Public" text={p.public.toString()} />
-                      <List.Item.Detail.Metadata.Separator />
-                      <List.Item.Detail.Metadata.Label title="Last Updated" text={dayjs(p.updatedAt).fromNow()} />
-                      <List.Item.Detail.Metadata.Separator />
-                      <List.Item.Detail.Metadata.Label title="Items" text={`${p.items.totalCount}`} />
-                      <List.Item.Detail.Metadata.Separator />
-                      <List.Item.Detail.Metadata.Label title="Views" text={`${p.views.totalCount}`} />
-                      <List.Item.Detail.Metadata.Separator />
-                    </List.Item.Detail.Metadata>
-                  }
-                />
-              }
-              actions={
-                <ActionPanel>
-                  <Action.OpenInBrowser title="Open Project" url={p.url} onOpen={onOpenProject} />
-                </ActionPanel>
-              }
-            />
-          ))}
+          {recentProjects
+            .filter((p) => !recentlyViewedProjectMap.get(p.url))
+            .map((p) => (
+              <List.Item
+                key={p.id}
+                title={p.title}
+                icon={{ source: "table.svg" }}
+                detail={
+                  <List.Item.Detail
+                    markdown={p.shortDescription}
+                    metadata={
+                      <List.Item.Detail.Metadata>
+                        <List.Item.Detail.Metadata.Label title="Public" text={p.public.toString()} />
+                        <List.Item.Detail.Metadata.Separator />
+                        <List.Item.Detail.Metadata.Label title="Last Updated" text={dayjs(p.updatedAt).fromNow()} />
+                        <List.Item.Detail.Metadata.Separator />
+                        <List.Item.Detail.Metadata.Label title="Items" text={`${p.items.totalCount}`} />
+                        <List.Item.Detail.Metadata.Separator />
+                        <List.Item.Detail.Metadata.Label title="Views" text={`${p.views.totalCount}`} />
+                        <List.Item.Detail.Metadata.Separator />
+                      </List.Item.Detail.Metadata>
+                    }
+                  />
+                }
+                actions={
+                  <ActionPanel>
+                    <Action.OpenInBrowser title="Open Project" url={p.url} onOpen={onOpenProject} />
+                  </ActionPanel>
+                }
+              />
+            ))}
         </List.Section>
       ) : (
         <List.EmptyView icon={{ source: "https://placekitten.com/500/500" }} title="No projects found" />
